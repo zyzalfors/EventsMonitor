c87eb4024615b1603246f69b6270be578afa5100
Al Armato <55386215+AleArmato@users.noreply.github.com>
2021-10-08 14:55:08 +0200

    Add files via upload

---
+++ MainForm.vb
@@ -0,0 +1,226 @@
+﻿Imports System.Collections.Specialized
+Imports System.IO
+Imports System.Runtime.InteropServices
+
+Public Class MainForm
+
+    Private Declare Function AddClipboardFormatListener Lib "user32.dll" (hWnd As IntPtr) As Boolean
+    Private Declare Function RemoveClipboardFormatListener Lib "user32.dll" (hWnd As IntPtr) As Boolean
+    Public Delegate Function KeyboardHookDelegate(Code As Int32, wParam As Int32, ByRef lParam As KBDLLHOOKSTRUCT) _
+    As Int32
+    Public Delegate Function MouseHookDelegate(Code As Int32, wParam As Int32, ByRef lParam As MSLLHOOKSTRUCT) _
+    As Int32
+    Private Declare Function SetWindowsHookKB Lib "user32.dll" Alias "SetWindowsHookExA" (idHook As Int32, lpfn As KeyboardHookDelegate, hmod As Int32, dwThreadId As Int32) As Int32
+    Private Declare Function SetWindowsHookMS Lib "user32.dll" Alias "SetWindowsHookExA" (idHook As Int32, lpfn As MouseHookDelegate, hmod As Int32, dwThreadId As Int32) As Int32
+    Private Declare Function UnhookWindowsHookEx Lib "user32.dll" (idHook As Int32) As Boolean
+    Private Declare Function MapVirtualKeyA Lib "user32.dll" (vkCode As Int32, mapType As Int32) As Int32
+    Private Const WM_CLIPBOARDUPDATE As Int32 = &H31D
+    Private Const WH_KEYBOARD_LL As Int32 = 13&
+    Private Const MAPVK_VK_TO_CHAR As Int32 = 2&
+    Private Const WM_KEYDOWN As Int32 = &H100
+    Private Const WM_LBUTTONDOWN As Int32 = &H201
+    Private Const WM_RBUTTONDOWN As Int32 = &H204
+    Private Const WM_SYSKEYDOWN As Int32 = &H104
+    Private Const WH_MOUSE_LL As Int32 = 14&
+    Private KBHook As Int32 = 0
+    Private MSHook As Int32 = 0
+    Private Const ProgramName As String = "Events Monitor Version 1.0.4"
+
+    Public Structure KBDLLHOOKSTRUCT
+        Public vkCode As Int32
+        Public scanCode As Int32
+        Public flags As Int32
+        Public time As Int32
+        Public dwExtraInfo As Int32
+    End Structure
+
+    Public Structure POINT
+        Public x As Int32
+        Public y As Int32
+    End Structure
+
+    Public Structure MSLLHOOKSTRUCT
+        Public pt As POINT
+        Public mouseData As Int32
+        Public flags As Int32
+        Public time As Int32
+        Public dwExtraInfo As Int32
+    End Structure
+
+    Private Function BytesToStringified(bytes As Byte()) As String
+        Dim stringified As String = ""
+        For Each octet In bytes
+            stringified += "0x" + Hex(octet) + " "
+        Next octet
+        Return stringified.Substring(0, stringified.Length - 1)
+    End Function
+
+    Private Function ListToStringified(strings As StringCollection)
+        Dim stringified As String = ""
+        For Each value In strings
+            stringified += value + Environment.NewLine
+        Next value
+        Return stringified.Substring(0, stringified.Length - 1)
+    End Function
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        If m.Msg = WM_CLIPBOARDUPDATE Then
+            Dim now = DateAndTime.Now.ToLocalTime
+            If My.Computer.Clipboard.ContainsText() Then
+                Dim description As String = "Copied text: " + My.Computer.Clipboard.GetText()
+                DataGridView.Rows.Add(now, description)
+                NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+            ElseIf My.Computer.Clipboard.ContainsImage Then
+                Dim img As Image = My.Computer.Clipboard.GetImage()
+                Dim bytes As Byte() = New ImageConverter().ConvertTo(img, GetType(Byte()))
+                Dim description As String = "Copied image: " + BytesToStringified(bytes)
+                DataGridView.Rows.Add(now, description)
+                NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+            ElseIf My.Computer.Clipboard.ContainsFileDropList Then
+                Dim description As String = "Copied/cutted: " + ListToStringified(My.Computer.Clipboard.GetFileDropList())
+                DataGridView.Rows.Add(now, description)
+                NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+            End If
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Protected Function KeyboardHookProc(Code As Int32, wParam As Int32, ByRef lParam As KBDLLHOOKSTRUCT) As Int32
+        Dim now = DateAndTime.Now.ToLocalTime
+        If wParam = WM_KEYDOWN Or wParam = WM_SYSKEYDOWN Then
+            Dim vkCode As Int32 = lParam.vkCode
+            Dim ch As String = If(MapVirtualKeyA(vkCode, MAPVK_VK_TO_CHAR) = 0, "0x" + Hex(vkCode), "0x" + Hex(vkCode) + " " + Chr(MapVirtualKeyA(vkCode, MAPVK_VK_TO_CHAR)))
+            Dim description As String = "Key pressed: " + ch
+            DataGridView.Rows.Add(now, description)
+            NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+        End If
+        Return 0
+    End Function
+
+    Protected Function MouseHookProc(Code As Int32, wParam As Int32, ByRef lParam As MSLLHOOKSTRUCT) As Int32
+        Dim now = DateAndTime.Now.ToLocalTime
+        If wParam = WM_LBUTTONDOWN Then
+            Dim x As String = lParam.pt.x
+            Dim y As String = lParam.pt.y
+            Dim description As String = "Mouse left key pressed: (" + x + " , " + y + ")"
+            DataGridView.Rows.Add(now, description)
+            NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+        ElseIf wParam = WM_RBUTTONDOWN Then
+            Dim x As String = lParam.pt.x
+            Dim y As String = lParam.pt.y
+            Dim description As String = "Mouse right key pressed: (" + x + " , " + y + ")"
+            DataGridView.Rows.Add(now, description)
+            NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+        End If
+        Return 0
+    End Function
+
+    Private Sub OnRenamed(sender As Object, e As RenamedEventArgs) Handles FileSystemWatcher.Renamed
+        Dim now = DateAndTime.Now.ToLocalTime
+        Dim description As String = "Renamed: " + e.OldFullPath + " --> " + e.FullPath
+        DataGridView.Rows.Add(now, description)
+        NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+    End Sub
+
+    Private Sub OnDeleted(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher.Deleted
+        Dim now = DateAndTime.Now.ToLocalTime
+        Dim description As String = "Deleted: " + e.FullPath
+        DataGridView.Rows.Add(now, description)
+        NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+    End Sub
+
+    Private Sub OnCreated(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher.Created
+        Dim now = DateAndTime.Now.ToLocalTime
+        Dim description As String = "Created: " + e.FullPath
+        DataGridView.Rows.Add(now, description)
+        NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+    End Sub
+
+    Private Sub OnChanged(sender As Object, e As FileSystemEventArgs) Handles FileSystemWatcher.Changed
+        Dim now = DateAndTime.Now.ToLocalTime
+        If e.ChangeType = WatcherChangeTypes.Changed Then
+            Dim description As String = "Changed: " + e.FullPath
+            DataGridView.Rows.Add(now, description)
+            NumEventsLabel.Text = (Convert.ToUInt64(NumEventsLabel.Text) + 1).ToString()
+        End If
+    End Sub
+
+    Public Function FormatPath(path As String) As String
+        Return If(path.EndsWith(":"), System.Text.RegularExpressions.Regex.Replace(path + "\", "\\+", "\"), System.Text.RegularExpressions.Regex.Replace(path, "\\+", "\"))
+    End Function
+
+    Private Sub StartEndDirMonitoring(sender As Object, e As EventArgs) Handles MonitorDirCheck.Click
+        If MonitorDirCheck.Checked Then
+            Dim path As String = FormatPath(DirText.Text.Trim())
+            If (Directory.Exists(path)) Then
+                Me.FileSystemWatcher.Path = path
+                Me.FileSystemWatcher.EnableRaisingEvents = True
+            Else
+                MessageBox.Show("Directory '" + path + "' not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                MonitorDirCheck.Checked = False
+            End If
+        Else
+            Me.FileSystemWatcher.EnableRaisingEvents = False
+        End If
+    End Sub
+
+    Private Sub ClearData(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DataGridView.Rows.Clear()
+        NumEventsLabel.Text = "0"
+    End Sub
+
+    Private Sub OpenSaveDialog(sender As Object, e As EventArgs) Handles SaveLabel.Click
+        SaveFileDialog.ShowDialog()
+    End Sub
+
+    Private Sub SaveFileOnOK(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog.FileOk
+        Dim FilePath As String = SaveFileDialog.FileName
+        Dim rows As DataGridViewRowCollection = DataGridView.Rows
+        Dim FileWriter As New System.IO.StreamWriter(FilePath)
+        FileWriter.WriteLine(ProgramName + Environment.NewLine)
+        For Each row In rows
+            FileWriter.WriteLine(row.Cells(0).Value + Environment.NewLine + row.Cells(1).Value + Environment.NewLine)
+        Next row
+        FileWriter.Close()
+        Dim choose As DialogResult = MessageBox.Show("File saved. Do you want to open it?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If choose = DialogResult.Yes Then Process.Start(FilePath)
+    End Sub
+
+    Private Sub ShowInfo(sender As Object, e As EventArgs) Handles InfoLabel.Click
+        Dim info As String = ProgramName + Environment.NewLine + "Developer: Al Armato" + Environment.NewLine + "Language: Visual Basic .NET"
+        MessageBox.Show(info, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub EnableSave(sender As Object, e As DataGridViewRowsAddedEventArgs) Handles DataGridView.RowsAdded
+        SaveLabel.Enabled = True
+        DataGridView.FirstDisplayedScrollingRowIndex = DataGridView.Rows.Count - 1
+    End Sub
+
+    Private Sub DisableSave(sender As Object, e As DataGridViewRowsRemovedEventArgs) Handles DataGridView.RowsRemoved
+        SaveLabel.Enabled = False
+    End Sub
+
+    Private Sub StartEndClipMonitoring(sender As Object, e As EventArgs) Handles MonitorClipCheck.Click
+        If MonitorClipCheck.Checked Then
+            AddClipboardFormatListener(MyBase.Handle)
+        Else
+            RemoveClipboardFormatListener(MyBase.Handle)
+        End If
+    End Sub
+
+    Private Sub StartEndKeyMonitoring(sender As Object, e As EventArgs) Handles MonitorKeyCheck.Click
+        If MonitorKeyCheck.Checked Then
+            KBHook = SetWindowsHookKB(WH_KEYBOARD_LL, New KeyboardHookDelegate(AddressOf KeyboardHookProc), 0, 0)
+        Else
+            UnhookWindowsHookEx(KBHook)
+        End If
+    End Sub
+
+    Private Sub MonitorMouseCheck_CheckedChanged(sender As Object, e As EventArgs) Handles MonitorMouseCheck.Click
+        If MonitorMouseCheck.Checked Then
+            MSHook = SetWindowsHookMS(WH_MOUSE_LL, New MouseHookDelegate(AddressOf MouseHookProc), 0, 0)
+        Else
+            UnhookWindowsHookEx(MSHook)
+        End If
+    End Sub
+End Class
